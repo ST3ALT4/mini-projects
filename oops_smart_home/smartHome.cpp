22a46c535f8542501c534c0fc60e01ff23cc2bfe
Anikai7 <anikait749@gmail.com>
2024-11-23 08:02:40 -0800

    v1.0

--- oops_smart_home/smartHome.cpp
+++ oops_smart_home/smartHome.cpp
@@ -41,7 +41,12 @@ public:
     }
 
     void status() override {
-        cout << "Light is " << (state ? "On" : "Off") << endl;
+        cout << "Light is ";
+        if (state) {
+            cout << "On" << endl;
+        } else {
+            cout << "Off" << endl;
+        }
     }
 };
 
@@ -61,7 +66,12 @@ public:
     }
 
     void status() override {
-        cout << "Thermostat is " << (state ? "On" : "Off");
+        cout << "Thermostat is ";
+        if (state) {
+            cout << "On";
+        } else {
+            cout << "Off";
+        }
         if (state) {
             cout << " and set to " << temperature << " degrees.";
         }
@@ -85,7 +95,12 @@ public:
     }
 
     void status() override {
-        cout << "Fan is " << (state ? "On" : "Off");
+        cout << "Fan is ";
+        if (state) {
+            cout << "On";
+        } else {
+            cout << "Off";
+        }
         if (state) {
             cout << " and set to speed " << speed << "%.";
         }
@@ -93,11 +108,10 @@ public:
     }
 };
 
-
 class smartHome {
 private:
     int deviceCount;
-    smartDevice* devices[10];  // Array to store pointers to devices
+    smartDevice* devices[10];
 
 public:
     smartHome() : deviceCount(0) {}
@@ -116,12 +130,65 @@ public:
         }
     }
 
-    void controlDevices() {
-        for (int i = 0; i < deviceCount; i++) {
-            devices[i]->turnOn();
-            devices[i]->status();
-            devices[i]->turnOff();
+    void deleteDevice() {
+        if (deviceCount > 0) {
+            delete devices[--deviceCount];
+            cout << "Device removed" << endl;
+        } else {
+            cout << "No devices to remove" << endl;
         }
+    }
+
+    void controlDevice(int index) {
+        if (index < 0 || index >= deviceCount) {
+            cout << "Invalid device index" << endl;
+            return;
+        }
+        int action;
+        do {
+            cout << "1.Turn On" << endl
+                << "2.Turn Off" << endl
+                << "3.Adjust Settings" << endl
+                << "4.Check Status" << endl
+                << "5.Back to Main Menu" << endl
+                << "Enter action for device " << index << ": ";
+            cin >> action;
+
+            switch (action) {
+                case 1:
+                    devices[index]->turnOn();
+                    break;
+                case 2:
+                    devices[index]->turnOff();
+                    break;
+                case 3:
+                    if (dynamic_cast<SmartLight*>(devices[index])) {
+                        int brightness;
+                        cout << "Enter brightness (0-100): ";
+                        cin >> brightness;
+                        dynamic_cast<SmartLight*>(devices[index])->adjustSettings(brightness);
+                    } else if (dynamic_cast<SmartThermostat*>(devices[index])) {
+                        int temp;
+                        cout << "Enter temperature: ";
+                        cin >> temp;
+                        dynamic_cast<SmartThermostat*>(devices[index])->adjustSettings(temp);
+                    } else if (dynamic_cast<SmartFan*>(devices[index])) {
+                        int speed;
+                        cout << "Enter speed (0-100): ";
+                        cin >> speed;
+                        dynamic_cast<SmartFan*>(devices[index])->adjustSettings(speed);
+                    }
+                    break;
+                case 4:
+                    devices[index]->status();
+                    break;
+                case 5:
+                    cout << "Returning to main menu..." << endl;
+                    break;
+                default:
+                    cout << "Invalid action, please try again." << endl;
+            }
+        } while (action != 5);
     }
 
     void monitorDevices() {
@@ -130,15 +197,58 @@ public:
         }
     }
 
+    void loop() {
+        int choice;
+        do {
+            cout << "1. Add Device (Light: 1, Thermostat: 2, Fan: 3)" << endl
+                << "2. Delete Last Device" << endl
+                << "3. Control Device" << endl
+                << "4. Monitor Devices" << endl
+                << "5. Exit" << endl
+                << "Enter your choice: ";
+            cin >> choice;
+
+            switch (choice) {
+                case 1: {
+                    int deviceType;
+                    cout << "Enter device type (1: Light, 2: Thermostat, 3: Fan): ";
+                    cin >> deviceType;
+                    if (deviceType == 1) {
+                        addDevice(new SmartLight());
+                    } else if (deviceType == 2) {
+                        addDevice(new SmartThermostat());
+                    } else if (deviceType == 3) {
+                        addDevice(new SmartFan());
+                    } else {
+                        cout << "Invalid device type" << endl;
+                    }
+                    break;
+                }
+                case 2:
+                    deleteDevice();
+                    break;
+                case 3: {
+                    int deviceIndex;
+                    cout << "Enter device index (0 to " << deviceCount - 1 << "): ";
+                    cin >> deviceIndex;
+                    controlDevice(deviceIndex);
+                    break;
+                }
+                case 4:
+                    monitorDevices();
+                    break;
+                case 5:
+                    cout << "Exiting..." << endl;
+                    break;
+                default:
+                    cout << "Invalid choice, please try again." << endl;
+            }
+        } while (choice != 5);
+    }
 };
 
 int main() {
     smartHome home;
-
-    home.addDevice(new SmartLight());
-    home.addDevice(new SmartThermostat());
-
-    home.controlDevices();
-
+    home.loop();
     return 0;
 }
